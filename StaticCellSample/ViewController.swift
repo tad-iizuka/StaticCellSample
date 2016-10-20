1a3087341ac46d5a34c863c3f797c0ab82b1e75e
tad-iizuka <tad.iizuka@gmail.com>
2016-10-20 09:59:14 +0900

    Initial Commit

---
+++ StaticCellSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StaticCellSample
+//
+//  Created by Tadashi on 2016/10/20.
+//  Copyright © 2016 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
